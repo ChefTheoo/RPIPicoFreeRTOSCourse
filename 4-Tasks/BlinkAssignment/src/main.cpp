2a9957dafe606a9168f95a8bf5479638f84704f7
ChefTheoo <t.khuoy@gmail.com>
2025-10-15 11:38:12 +0800

    Update main.cpp

--- 4-Tasks/BlinkAssignment/src/main.cpp
+++ 4-Tasks/BlinkAssignment/src/main.cpp
@@ -21,9 +21,10 @@
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD				0
-#define LED1_PAD			2
-#define LED2_PAD			3
+#define LED_PAD				2
+#define LED1_PAD			3
+#define LED2_PAD			4
+
 
 
 
@@ -94,7 +95,7 @@ void mainTask(void *params){
 
 	while (true) { // Loop forever
 		runTimeStats();
-		vTaskDelay(3000);
+		vTaskDelay(2000);
 	}
 }
 
@@ -122,12 +123,14 @@ int main( void )
 {
 	//Setup serial over USB and give a few seconds to settle before we start
     stdio_init_all();
-    sleep_ms(2000);
+    sleep_ms(3000);
     printf("GO\n");
 
     //Start tasks and scheduler
     const char *rtos_name = "FreeRTOS";
     printf("Starting %s on core 0:\n", rtos_name);
+    printf("KHUOY Théo F14158805\n");
+
     vLaunch();
 
 
